49a3fdf80492edb69ca786187dffeb6f19059a95
Christopher Batey <christopher.batey@gmail.com>
2014-09-06 15:46:51 +0100

    last but one

--- add.hs
+++ add.hs
@@ -3,3 +3,7 @@ add a b = a + b
 myDrop n xs = if n <= 0 || null xs
               then xs
               else myDrop (n - 1) (tail xs)
+
+lastButOne list = if (length list) <= 2
+				  then  head list        
+				  else  lastButOne (tail list)
